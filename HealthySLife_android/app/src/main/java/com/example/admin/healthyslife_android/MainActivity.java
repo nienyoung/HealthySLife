3b0e800d14048f148e6c0abf124920966cbd287b
Jingji-Wu <cx1327@hotmail.com>
2018-07-12 14:05:36 +0800

    add buttom nagivation view

--- HealthySLife_android/app/src/main/java/com/example/admin/healthyslife_android/MainActivity.java
+++ HealthySLife_android/app/src/main/java/com/example/admin/healthyslife_android/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.admin.healthyslife_android;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_exercise:
+                    mTextMessage.setText(R.string.title_exercise);
+                    return true;
+                case R.id.navigation_healthy:
+                    mTextMessage.setText(R.string.title_healthy);
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
