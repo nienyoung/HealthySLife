70e1ddab228f3a18151e5cc242930b7fdcc4e5a5
Jingji-Wu <cx1327@hotmail.com>
2018-07-19 15:00:07 +0800

    add calorie calculator

---
+++ HealthySLife_android/app/src/main/java/com/example/admin/healthyslife_android/utils/HealthyUils.java
@@ -0,0 +1,21 @@
+package com.example.admin.healthyslife_android.utils;
+
+/**
+ * @author wu jingji
+ */
+public class HealthyUils {
+    /**
+     * Calculate the calories burned in exercise
+     *
+     * @param weight   weight in kilogram(kg)
+     * @param time     exercise time in seconds
+     * @param velocity velocity in m/s
+     * @return burned calories
+     */
+    public float calorieCalculator(float weight, float time, float velocity) {
+        float k = 9 / (2 / velocity);
+        float hour = time / 3600;
+        float kcal = weight * hour * k;
+        return kcal * 1000;
+    }
+}
